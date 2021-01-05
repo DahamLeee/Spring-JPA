44515c41b4e22de761c1c3600721799a30479813
DahamLeee <dlek1011@gmail.com>
2021-01-05 23:34:08 +0900

    2. Entity Type Mapping

--- Day2/src/main/java/com/ssafy/demospringdata/Account.java
+++ Day2/src/main/java/com/ssafy/demospringdata/Account.java
@@ -1,8 +1,7 @@
 package com.ssafy.demospringdata;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 
 // 이 클래스가 데이터베이스에 있는 Account 테이블과 맵핑이 되는 것이라 알려주는거지
@@ -16,6 +15,15 @@ public class Account {
     private String username;
 
     private String password;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date = new Date();
+    // LocalTime 은 커스텀을 할 수 있대.. 그건 난이도 높음
+
+    private String yes;
+
+    @Transient
+    private String no;
 
     public Long getId() {
         return id;
